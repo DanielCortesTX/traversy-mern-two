04031ea4fb7dac56866d715c14412612a7e2c554
Daniel Cortes <d5t5c5@yahoo.com>
2019-06-30 12:23:28 -0500

    feat: Added Dashboard funcitonality and createprofile

---
+++ client/src/components/layout/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loading() {
+  return (
+    <div className="container pb-3 loading-size">
+      <h1 className="display-4 pb-3 text-center">Loading Projects...</h1>
+      <div className="loader pb-3"></div>
+    </div>
+  )
+}
