2b275d800beecdf58b8304b2c29c44d42beb3d08
Daniel Cortes <d5t5c5@yahoo.com>
2019-07-09 16:57:31 -0500

    feat: Added repos page

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from './ProfileTop'
 import ProfileAbout from './ProfileAbout'
 import ProfileExperience from './ProfileExperience'
 import ProfileEducation from './ProfileEducation'
+import ProfileGithub from './ProfileGithub'
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
   useEffect(() => {
@@ -52,7 +53,7 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
           </div>
 
           {profile.githubusername && (
-            <ProfileGithub />
+            <ProfileGithub username={profile.githubusername}/>
           )}
         </div>
       </Fragment>
